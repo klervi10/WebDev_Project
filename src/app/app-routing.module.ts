b69fd4ed44536c3ac497b88b99f488317813b153
klervi10 <148746828+klervi10@users.noreply.github.com>
2023-12-15 09:28:20 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 // app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component'; // Assurez-vous que cette ligne est présente
+import { HomeComponent } from './home/home.component'; 
 import { AboutComponent } from './about/about.component';
 import { StudentMealsComponent } from './student-meals/student-meals.component';
+import { ElaborateMealsComponent } from './elaborate-meals/elaborate-meals.component';
 
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'student', component: StudentMealsComponent },
+  { path: 'elaborate', component: ElaborateMealsComponent },
 ];
 
 @NgModule({
