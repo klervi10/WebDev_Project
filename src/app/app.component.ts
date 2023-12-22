421d62bde2cdd73f50ad589a543bb31438bd333c
Louise Dumont <louise.dumont03@outlook.fr>
2023-12-22 09:03:54 +0100

    change of size of images header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, HostListener } from '@angular/core';
-import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +8,20 @@ import { FormControl } from '@angular/forms';
 export class AppComponent {
   title = 'project_webdev_api_food';
   isComponentVisible = false;
- 
+
+  CarteMonde = 'assets/CarteMonde/CarteMonde.png';
+  GlobeLogo = 'assets/GlobeLogo/GlobeLogo.png';
+  Instagram = 'assets/Instagram/Instagram.png';
+  Linkedin = 'assets/Linkedin/Linkedin.png';
+  Mail = 'assets/Mail/Mail.png';
+  Facebook = 'assets/Facebook/Facebook.png';
+
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const windowHeight = window.innerHeight;
     const documentHeight = document.body.scrollHeight;
     const scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
- 
+
     this.isComponentVisible = documentHeight - windowHeight - scrollPosition <= 0;
   }
-
-
-
 }
-
