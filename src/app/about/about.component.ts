b5d93157971522649cdb63bf9a008b6e1ea6c859
Louise Dumont <louise.dumont03@outlook.fr>
2024-01-05 21:50:31 +0100

    ajout des commentaires english pour les fichiers about us

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,13 @@
+/* Import necessary Angular modules and services*/
 import { Component } from '@angular/core';
 
+/* Component decorator with metadata */
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: 'app-about', /* Angular component selector used in HTML */
+  templateUrl: './about.component.html', /* Path to the HTML template file */
+  styleUrls: ['./about.component.css'] /* Array of paths to external CSS style files */
 })
 export class AboutComponent {
-
+/* here, our component doesn't require any additional logic or properties so we leave it empty */
 }
 
